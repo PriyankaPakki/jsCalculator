98e0b9967794c553c3cc73b269863608bae902f7
priyanka19697 <priyanka19697@gmail.com>
2018-11-15 03:24:44 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,9 +1,62 @@
+//constructor to make a valid expression
 const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
+  displayValue: '0', //string representing user input or result of an operation
+  firstOperand: null, //first operand
+  waitingForSecondOperand: false, //flag to check if the expression is good to be evaluated
+  operator: null, //operator for expression
 };
+
+//to modify the calcy screen when any digits are clicked
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+
+  console.log(calculator);
+}
+
+//function to set the calculator screen to 0
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
+updateDisplay();
+
+//for handling (0-9) (+,-,/,*,=) (.) (AC) listen for clicks on calculator
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  //return if a target doesnt match a button
+  if (!target.matches('button')) {
+    return;
+  }
+  //if target is an operator handle it with function
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+		updateDisplay();
+    return;
+  }
+  // if target is a . take care
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+		updateDisplay();
+    return;
+  }
+  //if target is a AC
+  if (target.classList.contains('allclear')) {
+    resetCalculator();
+		updateDisplay();
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
 
 const performCalculation = {
   '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
@@ -21,18 +74,7 @@ function displayOperator(operator){
 	calculator.displayValue = operator;
 }
 
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
 
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-  }
-
-  console.log(calculator);
-}
 
 function inputDecimal(dot) {
   if (calculator.waitingForSecondOperand === true) return;
@@ -77,40 +119,7 @@ function resetCalculator() {
   calculator.operator = null;
 }
 
-function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
-}
 
-updateDisplay();
 
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-  const { target } = event;
-  if (!target.matches('button')) {
-    return;
-  }
 
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-		updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-		updateDisplay();
-    return;
-  }
-  
-
-  if (target.classList.contains('allclear')) {
-    resetCalculator();
-		updateDisplay();
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-});
 
